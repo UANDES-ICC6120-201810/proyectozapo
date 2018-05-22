8b4198369eb49ffb23a8ac029e9b30ebcdc7904d
dsebastian55 <sebaduranb@gmail.com>
2018-05-22 19:23:17 -0400

    install activestorage

--- app/models/file_to_update.rb
+++ app/models/file_to_update.rb
@@ -1,3 +1,15 @@
 class FileToUpdate < ApplicationRecord
   belongs_to :file_package
+  has_one_attached :file
+  validate :file_validation
+
+
+  private
+
+  def file_validation
+    if !file.attached?
+      file.purge
+      errors[:base] << "file attached must exist"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_18_135609) do
+ActiveRecord::Schema.define(version: 2018_05_21_175215) do
 
   create_table "access_group_bus_stops", force: :cascade do |t|
     t.integer "access_group_id"
@@ -67,6 +67,27 @@ ActiveRecord::Schema.define(version: 2018_05_18_135609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["bus_stop_id"], name: "index_access_points_on_bus_stop_id"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "bus_events", force: :cascade do |t|
