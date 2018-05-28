e70466f53c53c4047140bfe4aff23c39e90bfc0a
Jose Miguel Alvarez <jmalvarez@miuandes.cl>
2018-05-27 23:24:27 -0400

    adding auth to bus stop services endpoint and little change in access point form

--- app/controllers/api/v1/bus_stop_services.rb
+++ app/controllers/api/v1/bus_stop_services.rb
@@ -2,17 +2,14 @@ module API
   module V1
     class BusStopServices < Grape::API
       include API::V1::Defaults
+      #Auth
+      before {access_point_logger}
+
       resource :bus_stop_services do
         desc "Buses estimated waiting time"
-        params do
-          requires :token, type: String, desc: "Token of the Point"
-        end
-        get ":token", root: "bus_stop_services" do
-          accessPoint = AccessPoint.where(token: permitted_params[:token]).first!
-          if (accessPoint.present?)
-            busStopId = accessPoint.bus_stop_id
-            BusStopService.where(bus_stop_id: busStopId).first!
-          end
+        get "", root: :bus_stop_services do
+          busStopId = @current_access_point.bus_stop_id
+          BusStopService.where(bus_stop_id: busStopId)
         end
       end
     end
