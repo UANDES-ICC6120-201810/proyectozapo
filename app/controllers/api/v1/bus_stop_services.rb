729ff2e559201bc3c693992b9aad96bd4ea20d59
Jose Miguel Alvarez <jmalvarez@miuandes.cl>
2018-06-17 19:02:20 -0400

    estimation of buses endpoint update

--- app/controllers/api/v1/bus_stop_services.rb
+++ app/controllers/api/v1/bus_stop_services.rb
@@ -7,9 +7,13 @@ module API
 
       resource :estimation_of_buses do
         desc "Buses estimated waiting time"
-        get "", root: :bus_stop_services do
+        params do
+          requires :route_code, type: String, desc: "Service code"
+        end
+        get ":route_code", root: :bus_stop_services do
           busStopId = @current_access_point.bus_stop_id
-          BusStopService.where(bus_stop_id: busStopId, active: true)
+          service = Service.where(route_code: permitted_params[:route_code]).first!
+          BusStopService.where(bus_stop_id: busStopId, service_id: service.id, active: true)
         end
       end
     end
